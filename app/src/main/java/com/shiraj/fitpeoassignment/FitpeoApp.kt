5a4c5929cdd9b120e09edef57276577e307e4e20
shirajsayed <shirajsayed13@gmail.com>
2022-12-28 20:39:58 +0530

    Setup dependencies and other items

---
+++ app/src/main/java/com/shiraj/fitpeoassignment/FitpeoApp.kt
@@ -0,0 +1,8 @@
+package com.shiraj.fitpeoassignment
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FitpeoApp: Application() {
+}
--- app/src/main/java/com/shiraj/fitpeoassignment/MainActivity.kt
+++ app/src/main/java/com/shiraj/fitpeoassignment/MainActivity.kt
@@ -2,10 +2,17 @@ package com.shiraj.fitpeoassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shiraj.fitpeoassignment.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
